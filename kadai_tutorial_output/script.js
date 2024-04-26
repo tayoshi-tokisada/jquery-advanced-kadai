25ebd86c18739f69ba7539de5c353464dc7aced2
tayoshi-tokisada <mail.by.blacksheep@gmail.com>
2024-04-26 15:41:30 +0900

    lesson ~kadai_005

---
+++ kadai_tutorial_output/script.js
@@ -0,0 +1,24 @@
+$(function(){
+  // もっとみるボタンアニメーション
+  $('.button-more').on('mouseover', function(){
+    $(this).animate({
+      opacity: 0.5,
+      marginLeft: 20,
+    }, 100);
+  });
+   $('.button-more').on('mouseout', function(){
+     $(this).animate({
+       opacity: 1.0,
+       marginLeft: 0
+     }, 100);
+   });
+
+  //  メインビジュアルカルーセル
+  $(".carousel").slick({
+    autoplay: true,
+    dots: true,
+    Infinite: true,
+    autoplaySpeed: 5000,
+    arrows: false,
+  });
+});
