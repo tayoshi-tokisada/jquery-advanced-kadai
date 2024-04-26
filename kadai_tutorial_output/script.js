bad0a0e9024817c263656e9aed09f17092fa6d93
tayoshi-tokisada <mail.by.blacksheep@gmail.com>
2024-04-26 18:24:17 +0900

    change output

---
+++ kadai_tutorial_output/script.js
@@ -0,0 +1,15 @@
+$(function(){
+  // もっとみるボタンアニメーション
+  $(".button-more").on("mouseover", function(){
+    $(this).animate({
+      opacity: 0.5,
+      marginLeft: 20,
+    }, 100);
+  });
+  $(".button-more").on("mouseout", function(){
+    $(this).animate({
+      opacity: 1.0,
+      marginLeft: 0,
+    }, 100);
+  });
+});
